96e72774449b832da6bc713ee39efffffcdf401e
Tony Tsui <tsui.tony@gmail.com>
2012-12-17 00:17:52 +0000

    Only copy JPEG files

--- src/photo_picker/core.clj
+++ src/photo_picker/core.clj
@@ -4,15 +4,16 @@
 
 (defn foo [src-folder]
   (let [folders (.listFiles (File. src-folder))]
-    (dotimes [i-th-folder 5]
+    (dotimes [i-th-folder 100]
       (let [folder (rand-nth folders)
             files (.listFiles folder)]
          (println (str "folder: " folder))
-         (dotimes [j-th-file 2]
-           (let [file (rand-nth files)]
+         (dotimes [j-th-file 10]
+           (let [file (rand-nth files)
+                 extension (fs/extension file)]
              (println file)
-             (if (fs/file? file)
-               (fs/copy+ file (str "/home/tony-common/tmp/" i-th-folder "/" j-th-file ".jpg")))))))))
+             (if (and (fs/file? file) (= ".jpg" (.toLowerCase (fs/extension file))))
+               (fs/copy+ file (str "/home/tony/tmp/" i-th-folder "/" j-th-file ".jpg")))))))))
 
 
 ;(defn -main [args]
